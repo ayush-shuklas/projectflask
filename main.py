6c6dfe43cb205e8b42e11f3e7c974f5fcc4c1d99
Ayush Shukla <ayusshshuklaas@gmail.com>
2020-09-04 03:48:11 +0530

    fetching posts from database
    but not working correctly

--- main.py
+++ main.py
@@ -10,21 +10,20 @@ with open('config.json', 'r') as c:
     local_server = True
 app = Flask(__name__)
 app.config.update(
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = '465',
-    MAIL_USE_SSL = True,
-    MAIL_USERNAME = params['gmail-user'],
-    MAIL_PASSWORD=  params['gmail-password']
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-user'],
+    MAIL_PASSWORD=params['gmail-password']
 )
 mail = Mail(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flaskpost'
 
-if(local_server):
+if (local_server):
     app.config['SQLALCHEMY_DATABASE_URI'] = params['local_url']
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_url']
 db = SQLAlchemy(app)
-
 
 
 class Contacts(db.Model):
@@ -38,33 +37,54 @@ class Contacts(db.Model):
     date = db.Column(db.String(12), nullable=True)
     email = db.Column(db.String(20), nullable=False)
 
+    class Posts(db.Model):
+        sno = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(80), nullable=False)
+        slug = db.Column(db.String(21), nullable=False)
+        content = db.Column(db.String(120), nullable=False)
+        tagline = db.Column(db.String(120), nullable=False)
+        date = db.Column(db.String(12), nullable=True)
+        img_file = db.Column(db.String(12), nullable=True)
+
+
+
 @app.route('/')
 def home1():
-    return render_template('index.html',params=params)
+    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html', params=params)
 
 
 @app.route('/home')
 def home():
-    return render_template('index.html',params=params)
+    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
+    return render_template('index.html', params=params)
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html',params=params)
+    return render_template('about.html', params=params)
+
 
 @app.route("/post")
 def post():
-    return render_template('post.html',params=params)
+    return render_template('post.html', params=params)
 
 
-@app.route("/contact", methods = ['GET', 'POST'])
+@app.route("/post/<string:post_slug>", methods=['GET'])
+def post_route(post_slug):
+    post = Posts.query.filter_by(slug=post_slug).first()
+    return render_template('post.html', params=params, post=post)
+
+
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    if(request.method=='POST'):
+    if (request.method == 'POST'):
         '''Add entry to the database'''
         name = request.form.get('name')
         email = request.form.get('email')
         phone = request.form.get('phone')
         message = request.form.get('message')
-        entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
+        entry = Contacts(name=name, phone_num=phone, msg=message, date=datetime.now(), email=email)
         db.session.add(entry)
         db.session.commit()
         mail.send_message('New message from ' + name,
@@ -72,9 +92,7 @@ def contact():
                           recipients=[params['gmail-user']],
                           body=message + "\n" + phone
                           )
-    return render_template('contact.html',params=params)
+    return render_template('contact.html', params=params)
 
 
 app.run(debug=True)
-
-
