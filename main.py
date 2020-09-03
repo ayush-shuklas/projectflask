c4cc866d7caea05bd4f90cff55969af6c912cc66
Ayush Shukla <ayusshshuklaas@gmail.com>
2020-09-04 00:39:56 +0530

    project with flask
    added html,database connectivity using flasksql_alchemy,flask_mysqldb,and parameter configuration

---
+++ main.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import json
+from datetime import datetime
+
+with open('config.json', 'r') as c:
+    params = json.load(c)["params"]
+
+    local_server = True
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flaskpost'
+
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['local_url']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_url']
+db = SQLAlchemy(app)
+
+
+
+class Contacts(db.Model):
+    '''
+    sno, name phone_num, msg, date, email
+    '''
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
+
+@app.route('/')
+def home1():
+    return render_template('index.html',params=params)
+
+
+@app.route('/home')
+def home():
+    return render_template('index.html',params=params)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',params=params)
+
+@app.route("/post")
+def post():
+    return render_template('post.html',params=params)
+
+
+@app.route("/contact", methods = ['GET', 'POST'])
+def contact():
+    if(request.method=='POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name=name, phone_num = phone, msg = message, date= datetime.now(),email = email )
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html',params=params)
+
+
+app.run(debug=True)
+
+
